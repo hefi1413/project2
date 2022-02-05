b4aa31f675736ac07ecfe9bd440ef7ad49b9b509
hefi1413 <hefi1413@gmail.com>
2022-02-05 18:18:13 -0300

    atualizações

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-<<<<<<< HEAD
 /*
-
 Projeto 02
 
 O Jokenpô é o popular jogo do "Pedra, papel e tesoura". A premissa é simples, 
@@ -16,7 +14,6 @@ E para esse projeto você deve desenvolver um programa capaz de :
 - Receber o elemento escolhido pelo usuário;
 - Escolher um elemento aleatório para o computador;
 - Comparar os dois elementos e declarar um vencedor.
-
 */
 
 var prompt = require('prompt-sync')();
@@ -26,11 +23,9 @@ const JOGADOR1_ROUNDS = 0;
 const JOGADOR2_ROUNDS = 1;
 const EMPATE_ROUNDS = 2;
 
-// variáveis 
+// variáveis
 let jogo_elementos = ['pedra', 'tesoura', 'papel'];
 let jogo_rounds = [];
-let jogo_jogador1 = 0;
-let jogo_jogador2 = 1;
 let jogo_continuar = true;
 
 console.log();
@@ -137,77 +132,3 @@ function getRandom(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-=======
-
-var prompt =require('prompt-sync')();
-
-let history =
-        `   Se voce esta desempregado, ja tem +30 e/ou problemas na área da sáude , certamente esta se sentindo por baixo, desmotivado e inseguro.
-
-            Contudo, você ainda sonha com uma residência em um bairro tranquilo , uma boa renda por mês e uma família sob sua responsabilidade ... 
-
-            Mas, como conseguir meterializar estes sonhos na atual realidade ?
-
-            Então, ainda com estas questões em mente, vc abre seu cliente de email e percebe que irá ocorrer um concurso público e que será uma ótima opção para você.
-
-            Isso mesmo !!! vc exclama. Essa é minha oportunidade!  
-    
-            Dessa maneira, já decidido, vc começa a se aplicar para passar no concurso, se tornar um servidor público e iniciar sua jornada. :)`;
-
-    console.log();
-    console.log();
-    console.info( history );
-    console.log();
-    console.info("A Jornada do Herói");
-    console.log("---------------------------");
-    console.log();
-    console.info("Segue abaixo 5 perguntas relacionadas ao seu planejamento de esctudo:");
-    console.log();
-
-    let quest1 = prompt("1- Você sabe que deverá investir uma grana para adiquirir um bom curso/apostila para treinar ?");
-    let quest2 = prompt("2- Ja comprende que deverá utilizar alguma técnica de memorização para 1/5 de questões impossíveis de se lembrar ?");
-    let quest3 = prompt("3- Entende que deverá montar um ciclo de estudo para as disciplinas envolvidas?");
-    let quest4 = prompt("4- Tmabém sabe que deverá abdicar de passeios/descanços em alguns finais de semana ?");
-    let quest5 = prompt("5- Por final, como um bom concurseiro, compreende que uma alimentação saudável também melhora sua concentração e aprendizado ?");
-
-    var soma =0;
-    if(quest1.toUpperCase()=="S" ) {
-        soma += 1;
-    }
-
-    if(quest2.toUpperCase()=="S" ) {
-        soma += 1;
-    }
-
-    if(quest3.toUpperCase()=="S" ) {
-        soma += 1;
-    }
-
-    if(quest4.toUpperCase()=="S" ) {
-        soma += 1;
-    }
-
-    if(quest5.toUpperCase()=="S" ) {
-        soma += 1;
-    }
-
-    console.log();
-    console.info('Resultado');
-    console.log("---------------------------");
-
-    if (soma == 0 ) {
-        console.info("Você falha miseravelmente ...");
-    }
-    else if(soma==1 || soma==2 ) {
-        console.info("Você falha ,mas consegue desfaçar.");
-    }
-    else if( soma == 3 ) {
-        console.info("Você chega perto, mas falha por pouco");
-    }
-    else if( soma == 4 ) {
-        console.info("Depois de muito esforço vc consegue seu objetivo, porém não de forma perfeita");
-    }
-    else if(soma == 5 ) {
-        console.info("Você triunfa de forma inquestionavél e seus feitos serão lembrados por gerações !!");
-    }
->>>>>>> 0fda704f6f8fe570dadbc7089225b9f3f88bb1e5
